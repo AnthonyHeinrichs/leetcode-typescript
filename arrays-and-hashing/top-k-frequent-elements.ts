8cb66270f56853bde2e1a918cda5b5830305e4f5
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-08-10 10:40:23 +0200

    Add solution for top-k-frequent-elements

--- arrays-and-hashing/top-k-frequent-elements.ts
+++ arrays-and-hashing/top-k-frequent-elements.ts
@@ -2,5 +2,15 @@
 You may return the answer in any order. */
 
 function topKFrequent(nums: number[], k: number): number[] {
-  return []
+  const counts: {[index: string]:any} = {};
+
+  // O(n)
+  for (const num of nums) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  // O(n log n)
+  const sortedKeys = Object.keys(counts).map(Number).sort((a, b) => counts[b] - counts[a]);
+  
+  return sortedKeys.slice(0, k)
 }
